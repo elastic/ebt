cd5fb9df2137a44834504b07a45046f2f06b5084
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2025-07-09 16:24:35 +0200

    Remove leaked helpers dir

--- helpers/global_session/global_session_context_provider.d.ts
+++
@@ -1,7 +0,0 @@
-import { type Subject } from 'rxjs';
-import { AnalyticsClient } from '../../client';
-export interface GlobalSessionContextProviderOpts {
-    analyticsClient: Pick<AnalyticsClient, 'registerContextProvider'>;
-    userId$: Subject<string>;
-}
-export declare function registerGlobalSessionContextProvider({ analyticsClient, userId$ }: GlobalSessionContextProviderOpts): void;
